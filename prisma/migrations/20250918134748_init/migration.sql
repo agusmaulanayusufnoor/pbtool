80b8e7b7c94b6b2c23fff32df689f783bd718826
Agus Maulana Yusuf Noor <agusmaulanayusufnoor@gmail.com>
2025-09-20 17:11:32 +0700

    update db to supabase

--- prisma/migrations/20250918134748_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Setting" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "userId" TEXT NOT NULL,
-    "cardNumber" TEXT NOT NULL,
-    "email" TEXT NOT NULL
-);
--- prisma/migrations/20250918155104_add_unique_email_to_setting/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email]` on the table `Setting` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Setting_email_key" ON "Setting"("email");
