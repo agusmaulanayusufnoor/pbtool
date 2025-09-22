7a175107769aa0636c8d7272a1e000c0348cec26
Agus Maulana Yusuf Noor <agusmaulanayusufnoor@gmail.com>
2025-09-22 16:09:32 +0700

    update

--- src/features/auth/components/user-auth-form.tsx
+++ src/features/auth/components/user-auth-form.tsx
@@ -1,12 +1,7 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
+  Form
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
